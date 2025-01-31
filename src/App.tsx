4869d17c09cf5e16080efec4d3d6d4c3407f57e9
Tushar Shinde <tusharshindets6.com>
2025-01-31 15:57:19 +0530

    Build a responsive layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,32 @@
-import { Button, ButtonGroup } from "@chakra-ui/react"
+import { Grid, GridItem, Show } from "@chakra-ui/react";
+
+const templateAreas = {
+  base: `"nav" "main"`,
+  lg: `"nav nav" "aside main"`,
+};
 
 function App() {
- return <button colorPalette="blue">Button</button>;
-  
+  return (
+    <Grid
+      templateAreas={templateAreas}
+      gridTemplateColumns={{ base: "1fr", lg: "600px 1fr" }}
+    gridTemplateRows="auto"
+      //gap={4}
+      p={4}
+    >
+      { <GridItem gridArea="nav" bg="coral" p={4}>
+        Nav
+      </GridItem> }
+      <Show above="lg">
+        <GridItem gridArea="aside" bg="gold" p={4}>
+          Aside
+        </GridItem>
+      </Show>
+      <GridItem gridArea="main" bg="dodgerblue" p={4}>
+        Main
+      </GridItem>
+    </Grid>
+  );
 }
 
-export default App
+export default App;
