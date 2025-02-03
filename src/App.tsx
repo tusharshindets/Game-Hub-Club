4e1b2ec50627ae13a478450413d701a4968f8be9
Tushar Shinde <tusharshindets6.com>
2025-02-03 11:24:20 +0530

    feat: creat responsive layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,17 @@
-import { Grid, GridItem, Show } from "@chakra-ui/react";
-
-const templateAreas = {
-  base: `"nav" "main"`,
-  lg: `"nav nav" "aside main"`,
-};
+import { Grid, GridItem } from "@chakra-ui/react"
 
 function App() {
-  return (
-    <Grid
-      templateAreas={templateAreas}
-      gridTemplateColumns={{ base: "1fr", lg: "600px 1fr" }}
-    gridTemplateRows="auto"
-      //gap={4}
-      p={4}
-    >
-      { <GridItem gridArea="nav" bg="coral" p={4}>
-        Nav
-      </GridItem> }
-      <Show above="lg">
-        <GridItem gridArea="aside" bg="gold" p={4}>
-          Aside
-        </GridItem>
-      </Show>
-      <GridItem gridArea="main" bg="dodgerblue" p={4}>
-        Main
+  return <Grid templateAreas={'"nav nav" "aside main"'}>
+    <GridItem area='nav' bg='coral'>
+      Nav
       </GridItem>
-    </Grid>
-  );
+    <GridItem area='aside' bg='gold'>
+      Aside
+      </GridItem>
+    <GridItem area='main' bg='dodgerblue'>
+      Main
+      </GridItem>
+     </Grid>
 }
 
-export default App;
+export default App 
