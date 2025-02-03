792ee448f2cf80de3f332a1ae82226db72eabd2e
Tushar Shinde <tusharshindets6.com>
2025-02-03 12:19:49 +0530

    Build a navbar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,10 +1,11 @@
-import { HStack, Image } from "@chakra-ui/react";
-import React from "react";
-
+import { HStack, Image, Text } from "@chakra-ui/react";
+//import React from "react";
+import logo from '../assets/logo.png';
 const NavBar = () => {
     return (
         <HStack>
-            {/* <Image scr={} /> */}
+             <Image src={logo} boxSize='60px' /> 
+             <Text>NavBar</Text>
         </HStack>
     )
 }
