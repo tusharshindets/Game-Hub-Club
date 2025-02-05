fdfc91a123a017d6b3b3e5734405d47c72c210f0
Tushar Shinde <tusharshindets6.com>
2025-02-05 12:16:45 +0530

    Displaying critic score

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import { Game } from '../hooks/useGames'
-import { Card, CardBody, Heading, Image, Text } from '@chakra-ui/react'
+import { Card, CardBody, Heading, HStack, Image, Text } from '@chakra-ui/react'
 import PlatformIconList from './PlatformIconList'
+import CriticScore from './CriticScore'
 
 interface props {
     game: Game
@@ -13,8 +14,11 @@ const GameCard = ({ game }: props) => {
             <Image src={game.background_image} />
             <CardBody>
                 <Heading fontSize='2xl'>{game.name}</Heading>
+                <HStack justifyContent='space-between'>
               {/*  {game.parent_platforms.map( ({platform}) => <Text>{platform.name}</Text>)} */}
               <PlatformIconList platforms={game.parent_platforms.map(p => p.platform)} />
+                <CriticScore score={game.metacritic} />
+                 </HStack>
             </CardBody>
         </Card>
     )
--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -12,7 +12,8 @@ export interface Game {
     id: number;
     name: string;
     background_image: string;
-    parent_platforms: { platform: platform}[]
+    parent_platforms: { platform: platform}[];
+    metacritic: number;
 }
 
 interface FetchGamesResponse {
