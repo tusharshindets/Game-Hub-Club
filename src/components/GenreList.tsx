b06c8e6f522c7fd35addfcaa2522edf5808f42bc
Tushar Shinde <tusharshindets6.com>
2025-02-07 17:20:55 +0530

    Clean up the genres

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,5 +1,5 @@
 
-import { HStack, List, ListItem, Image, Text, Spinner, Button } from "@chakra-ui/react";
+import { HStack, List, ListItem, Image, Text, Spinner, Button, Heading } from "@chakra-ui/react";
 import useGenres, { Genre } from "../hooks/useGenres";
 import getCroppedImageUrl from "../services/image-url";
 
@@ -16,6 +16,8 @@ const GenreList = ({ selectedGenre, onSelectGenre }: Props) => {
     if (isLoading) return <Spinner />;
 
     return(
+            <>
+        <Heading fontSize='2xl' marginBottom={3}>Genres</Heading>
         <List>
             {data.map(genre => (
                  <ListItem key={genre.id} paddingY='5px'>
@@ -23,17 +25,18 @@ const GenreList = ({ selectedGenre, onSelectGenre }: Props) => {
                    <Image
                     boxSize='32px'
                      borderRadius={8}
+                     objectFit='cover'
                       src={getCroppedImageUrl ( genre.image_background)}
                        />
-                       <Button fontWeight={genre.id === selectedGenre?.id ? 'bold' : 'normal'} onClick={() => onSelectGenre(genre) } fontSize='lg' variant='link'>
+                       <Button whiteSpace='normal' textAlign='left' fontWeight={genre.id === selectedGenre?.id ? 'bold' : 'normal'} onClick={() => onSelectGenre(genre) } fontSize='lg' variant='link'>
                         {genre.name}
                        </Button>
                        </HStack>
                 </ListItem>
             ))}
         </List>
-    )
-
-}
+        </>
+    );
+};
 
 export default GenreList;
--- src/components/SearchInput.tsx
+++ src/components/SearchInput.tsx
@@ -1,4 +1,4 @@
-import { Input, InputGroup, InputLeftElement } from "@chakra-ui/react";
+{/*import { Input, InputGroup, InputLeftElement } from "@chakra-ui/react";
 import { useRef  } from "react";
 import { BsSearch } from "react-icons/bs";
 
@@ -34,5 +34,39 @@ const SearchInput = ({ onSearch }: Props) => {
     );
 };
 
-export default SearchInput;
+export default SearchInput;*/}
 
+import { Input, InputGroup, InputLeftElement } from "@chakra-ui/react";
+import { useRef } from "react";
+import { BsSearch } from "react-icons/bs";
+
+interface Props {
+    onSearch: (searchText: string) => void;
+}
+
+const SearchInput = ({ onSearch }: Props) => {
+    const ref = useRef<HTMLInputElement>(null); // ✅ Declare ref at the top level
+
+    return (
+        <form
+            onSubmit={(event) => {
+                event.preventDefault();
+                if (ref.current) onSearch(ref.current.value);
+            }}
+        >
+            <InputGroup>
+                <InputLeftElement pointerEvents="none">
+                    <BsSearch />
+                </InputLeftElement>
+                <Input
+                    ref={ref}
+                    borderRadius={20}
+                    placeholder="Search games..."
+                    variant="filled"
+                />
+            </InputGroup>
+        </form>
+    );
+};
+
+export default SearchInput;
